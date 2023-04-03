2577b272ddf9af7713196b0bcb00dde3984f6aee
Edward Yamamoto <kfcs700@gmail.com>
2023-04-03 22:11:22 +0800

    Got Blink Working on Circuit Board

--- software-design/cmake/atmega-8a-device.cmake
+++ software-design/cmake/atmega-8a-device.cmake
@@ -1,6 +1,6 @@
 set(DEVICE "atmega8")
 set(CLOCK "16000000")
 
-set(L_FUSE "0b11110111")
-set(H_FUSE "0b10011001")
-set(E_FUSE "0x11111111")
+set(L_FUSE "0xEF")
+set(H_FUSE "0xC9")
+set(E_FUSE "0xFF")
--- software-design/src/main.cpp
+++ software-design/src/main.cpp
@@ -1,17 +1,18 @@
+#define __AVR_ATmega8A__ ;
 #include <avr/builtins.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(int argc, char const *argv[])
 {
     __builtin_avr_cli();
-
+    DDRB = 0xFF;
     __builtin_avr_sei();
 
-    while (1)
+    while (true)
     {
-        // do something
+        PORTB ^= 0xFF;
+        _delay_ms(1000);
     }
-
-    return 0;
 }
