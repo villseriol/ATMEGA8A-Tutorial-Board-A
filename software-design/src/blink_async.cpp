00af4a31c721357bac66a63bd2c6254f55dd899d
Edward Yamamoto <kfcs700@gmail.com>
2023-04-08 21:13:38 +0800

    Completed Blink Async

--- software-design/src/blink_async.cpp
+++ software-design/src/blink_async.cpp
@@ -3,7 +3,6 @@
 #include <avr/builtins.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <util/delay.h>
 
 #include "common.h"
 
@@ -11,9 +10,26 @@ int main(void)
 {
     common::setup();
 
+    __builtin_avr_cli();
+    TCCR1A = _BV(COM1A0) | _BV(COM1B0);
+    TCCR1B = _BV(CS12);
+    TIMSK = _BV(OCIE1A) | _BV(OCIE1B);
+
+    OCR1A = 0xFFFF;
+    OCR1B = 0xFFFF;
+
+    __builtin_avr_sei();
+
     while (1)
     {
-        PORTD ^= 0xFF;
-        _delay_ms(1000);
+        continue;
     }
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+}
+
+ISR(TIMER1_COMPB_vect)
+{
 }
