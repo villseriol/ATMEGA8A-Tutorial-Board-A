6e28b087efa3c7592c54c7e4162e868554e9c136
Edward Yamamoto <kfcs700@gmail.com>
2023-04-09 23:15:17 +0800

    Completed 7 Segment Code

--- software-design/src/7_segment.cpp
+++ software-design/src/7_segment.cpp
@@ -11,7 +11,7 @@
 namespace context
 {
     volatile uint8_t digits[] = {0xFF, 0xFF, 0xFF, 0xFF};
-    volatile uint8_t current = 0;
+    const uint8_t digits_size = sizeof(context::digits) / sizeof(*context::digits);
 } // namespace context
 
 const unsigned char SEGMENT_NUMBER[] PROGMEM = {
@@ -33,7 +33,7 @@ const unsigned char SEGMENT_NUMBER[] PROGMEM = {
     (unsigned char)~0b01001101, // F
 };
 
-void itom(const uint16_t value, uint8_t *buffer)
+void ito7(const uint16_t value, uint8_t *buffer)
 {
     uint16_t quotient = value;
     uint16_t remainder = 0;
@@ -50,23 +50,7 @@ void print(const uint16_t number)
 {
     uint8_t buffer[] = {0xFF, 0xFF, 0xFF, 0xFF};
     const uint8_t buffer_size = sizeof(buffer) / sizeof(*buffer);
-    itom(number, buffer);
-
-    for (uint8_t i = 0; i < buffer_size; i++)
-    {
-        context::digits[i] = buffer[i];
-    }
-
-    context::current = 0;
-
-    // PORTD = digit;
-    // PORTC &= ~(_BV(PC0) | _BV(PC1) | _BV(PC2) | _BV(PC3));
-}
-
-void clear()
-{
-    uint8_t buffer[] = {0xFF, 0xFF, 0xFF, 0xFF};
-    const uint8_t buffer_size = sizeof(buffer) / sizeof(*buffer);
+    ito7(number, buffer);
 
     for (uint8_t i = 0; i < buffer_size; i++)
     {
@@ -79,16 +63,33 @@ int main(void)
     common::setup();
 
     __builtin_avr_cli();
-    // setup 256 bit timer as display
+
+    TIMSK |= _BV(TOIE0);
+    TCCR0 = _BV(CS02);
 
     __builtin_avr_sei();
 
-    unsigned counter = 0;
+    uint16_t counter = 0;
 
     while (1)
     {
-        print(counter++);
-        counter %= 10;
+        counter++;
+        print(counter);
+        counter %= 10000;
         _delay_ms(1000);
     }
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    static uint8_t digit_selection_number = 0;
+
+    const uint8_t mask = _BV(PC0) | _BV(PC1) | _BV(PC2) | _BV(PC3);
+    const uint8_t digit_selection_mask = (PORTC | mask) & ~_BV(digit_selection_number);
+    const uint8_t digit_selection_index = context::digits_size - digit_selection_number - 1;
+
+    PORTD = context::digits[digit_selection_index];
+    PORTC = digit_selection_mask;
+
+    digit_selection_number = ++digit_selection_number % context::digits_size;
 }
